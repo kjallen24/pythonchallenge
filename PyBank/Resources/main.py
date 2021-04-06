3d5da621dc4cca821bbc19d3be2aa65d276a1d54
kallen <kadatagirl@gmail.com>
2021-04-05 20:58:45 -0500

    push again

---
+++ PyBank/Resources/main.py
@@ -0,0 +1,28 @@
+#Financial Analysis Code w/ Notes: Attachments- Budget_Data.csv
+# The total number of months included in the dataset []
+# The net total amount of "Profit/Losses" over the entire period
+# The average of the changes in "Profit/Losses" over the entire period
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in losses (date and amount) over the entire period
+
+import os
+import csv
+
+print (os.getcwd())
+
+Budget_Data = os.path.join('..','Resources', 'Budget_Data.csv')
+with open('Budget_Data', 'r') as f:
+    budgetreader = csv.reader(f)
+    print(budgetreader)
+    for row in budgetreader:
+        print (row)
+        
+        #file_one_df= pd.read_csv
+
+        #Print("Financial Analysis")
+        #Print("-------------------")
+        #Print(f"Total Months {}")
+        #Print()
+        
+        
+
