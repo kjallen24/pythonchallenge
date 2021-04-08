c9f44ddbe476e17da4005288ddb9c77568ba5e35
kallen <kadatagirl@gmail.com>
2021-04-08 13:39:06 -0500

    updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,6 @@
 #Financial Analysis Code w/ Notes: Attachments- Budget_Data.csv
-# The total number of months included in the dataset []
-# The net total amount of "Profit/Losses" over the entire period
+# The total number of months included in the dataset [x]
+# The net total amount of "Profit/Losses" over the entire period [x]
 # The average of the changes in "Profit/Losses" over the entire period
 # The greatest increase in profits (date and amount) over the entire period
 # The greatest decrease in losses (date and amount) over the entire period
@@ -10,10 +10,21 @@ import csv
 
 Budget_Data = os.path.join('Resources', 'Budget_Data.csv')
 
-with open(Budget_Data, 'r') as csvfile:
-    cvsreader = csv.reader(csvfile, delimiter=',')
-    lines = len(list(cvsreader))
-    print (lines-1)
+with open(Budget_Data) as financial_data:
+    reader = csv.reader(financial_data)
+    header = next(reader)
+
+    months = 0
+    net = 0
+    for row in reader:
+        months += 1
+        net += int(row[1])
+    
+print ("Financial Analysis")
+print("------------------------")
+print (f"Total Months: {months}")
+print (f"Total: ${net}")
+print (f"Average Change: $")
 
 #Final_Analysis = zip(Date,TProfitLoss, PLAverage, Increase, Decrease)
 
