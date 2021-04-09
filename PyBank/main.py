5aeb5e0c0629f6161caa76591b7d3f4dac8048f0
kallen <kadatagirl@gmail.com>
2021-04-09 14:40:01 -0500

    new code

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,15 +16,37 @@ with open(Budget_Data) as financial_data:
 
     months = 0
     net = 0
-    for row in reader:
+    diff = 0
+    first_row = next(reader)
+    #diff_list = []
+    prev_net = int(first_row[1])
+    greatest = 0
+    least = 0  
+    
+    for i, row in enumerate(reader):
         months += 1
-        net += int(row[1])
+        net += int(row[1])     
+        net_change = int(first_row[i + 1]) - prev_net
+        average = net_change / 86
+        if prev_net > net_change:
+            
+        elif prev_net < net_change:
+            prev_net = least
+        first_row += [1] 
+        prev_net = int(row[1])
+        
+
+
+        
     
 print ("Financial Analysis")
 print("------------------------")
 print (f"Total Months: {months}")
 print (f"Total: ${net}")
-print (f"Average Change: $")
+print (f"Average Change: ${average}")
+print (f"Greatest Increase in Profits:{greatest}")
+print (f"Greatest Decrease in Profits:{least}")
+print("------------------------")
 
 #Final_Analysis = zip(Date,TProfitLoss, PLAverage, Increase, Decrease)
 
@@ -36,8 +58,7 @@ print (f"Average Change: $")
     #writer = csv.writer(datafile)
 
     # Write the header row
-    #writer.writerow(["Title", "Course Price", "Subscribers", "Reviews Left",
-                     #"Percent of Reviews", "Length of Course"])
+    #writer.writerow(["Financial Analysis"])
 
     # Write in zipped rows
     #writer.writerows(cleaned_csv)
